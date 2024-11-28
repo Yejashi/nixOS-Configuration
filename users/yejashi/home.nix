13ecd138fd2041f560962c7c38535ad29a777d2c
yejashi <befikirteferi@gmail.com>
2024-11-28 14:01:00 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -108,6 +108,13 @@
     };
   };
 
+  # TODO: What to do with this?
+#   programs.python = {
+#     enable = true;
+#     package = pkgs.python312;
+#   };
+
+  
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
