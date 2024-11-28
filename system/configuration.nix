7dcf7546b46bed7169452bbb8bc09186835b6652
yejashi <befikirteferi@gmail.com>
2024-11-28 11:32:13 -0500

    minor

--- system/configuration.nix
+++ system/configuration.nix
@@ -145,7 +145,6 @@
      # Add zen-browser later on
   ];
 
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
