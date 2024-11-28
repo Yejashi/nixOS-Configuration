fcd55ee4d93e8ea1b62c08ac9fc897d8f0b237bf
yejashi <befikirteferi@gmail.com>
2024-11-28 12:47:55 -0500

    minor

--- system/configuration.nix
+++ system/configuration.nix
@@ -170,7 +170,7 @@
       # package.disabled = true;
     };
 
-    # presets = [];
+    presets = [ "tokyo-night" ];
   };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
