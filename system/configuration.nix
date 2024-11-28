6d221bb4e43890a3997e5130a7a60081dd306c88
yejashi <befikirteferi@gmail.com>
2024-11-28 14:43:11 -0500

    minor

--- system/configuration.nix
+++ system/configuration.nix
@@ -183,7 +183,7 @@
     presets = [ "tokyo-night" ];
   };
 
-  services.inputRemapper.enable = true;
+#   services.inputRemapper.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
