8963497f565144c22c99d7d2da9a6a80c1065fa5
yejashi <befikirteferi@gmail.com>
2024-11-28 10:53:28 -0500

    minor

---
+++ users/yejashi/rofi/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cp config.rasi ~/.config/rofi
+
+cp power.rasi ~/.config/rofi
+
+git clone https://github.com/catppuccin/rofi ./.catppuccin-rofi
+
+cd ./.catppuccin-rofi
+
+mkdir -p ~/.config/rofi ~/.local/share/rofi/themes
+
+cp -r ./.config/rofi/* ~/.config/rofi
+
+cp -r ./.local/share/rofi/themes/* ~/.local/share/rofi/themes/
+
+
+
---
+++ users/yejashi/user.js
@@ -0,0 +1,17 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // for enable userChrome/userContent
+user_pref("svg.context-properties.content.enabled", true); // for svg
+user_pref("layout.css.color-mix.enabled", true); // for color-mix
+
+// * Available preferences
+
+// user_pref("userChrome.chrome-refresh-2023", true);
+
+// * Color themes, use only one
+// user_pref("userChrome.chrome-refresh-colors", true);
+// user_pref("userChrome.default-theme-colors", true);
+// user_pref("userChrome.dracula-theme-colors", true);
+// user_pref("userChrome.github-theme-colors", true);
+
+// * Force enable control animation, because by default respects the user animation disable preference.
+// * (Not required if you do not disable animation)
+// user_pref("userChrome.force-enable-animations", true);
