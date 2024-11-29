3bfd37c99a9b4b9df29c3fc0ec4e0b1a5b956440
yejashi <befikirteferi@gmail.com>
2024-11-28 21:34:50 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -154,6 +154,11 @@
         name = "Orchis-Dark";
     };
 
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+    ];
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = {
         "custom0" = {
             name = "Rofi";
