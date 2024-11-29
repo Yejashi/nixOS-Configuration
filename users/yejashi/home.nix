249d94d13c33aec9d1e345369bf65a88b5548fcb
yejashi <befikirteferi@gmail.com>
2024-11-28 20:29:52 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -132,7 +132,7 @@
   home.activation.copyUserJS = lib.mkAfter ''
     for profile in ~/.mozilla/firefox/*.default; do
       install -Dm644 ${./user.js} "$profile/user.js"
-      install -Dm644 ${./user.js} "$profile/chrome"
+      install -Dm644 ${./chrome} "$profile/chrome"
     done
   '';
 
