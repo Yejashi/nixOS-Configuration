d45e8f9318b7b124345095009c08ff68bc2aa01a
yejashi <befikirteferi@gmail.com>
2024-11-28 20:47:28 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -138,10 +138,10 @@
       for file in ${./chrome}/*; do
         install -m 644 "$file" "$profile/chrome/"
       done
-      for file in ${./chrome/fonts}/*; do
+      for file in ./chrome/fonts/*; do
         install -m 644 "$file" "$profile/chrome/fonts"
       done
-      for file in ${./chrome/icons}/*; do
+      for file in ./chrome/icons/*; do
         install -m 644 "$file" "$profile/chrome/icons"
       done
     done
