81fa50869cd3bae8cad5d48768112470e491f9de
yejashi <befikirteferi@gmail.com>
2024-11-28 19:04:56 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -150,11 +150,12 @@
       "org/gnome/shell/extensions/user-theme" = {
         name = "Orchis-Dark";
       };
+
+      "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+      };
     };
 
-    "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-    };
 
     "org/gnome/desktop/wm/preferences" = {
         button-layout = "appmenu:minimize,maximize,close";
