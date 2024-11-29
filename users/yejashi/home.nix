ce46d3afe117fb2270f0873cc0918211fe255d31
yejashi <befikirteferi@gmail.com>
2024-11-29 11:31:50 -0500

    minor

--- users/yejashi/home.nix
+++ users/yejashi/home.nix
@@ -206,32 +206,15 @@
         };
     };
 
-  # TODO: What to do with this?
-#   programs.python = {
-#     enable = true;
-#     package = pkgs.python312;
-#   };
-  
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/yejashi/etc/profile.d/hm-session-vars.sh
-  #
+
   home.sessionVariables = {
     EDITOR = "vim";
   };
 
-  ## Manage dot files here: store files in github and create symlinks where they are necessary
+  home.activation.postBuildScript = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ ! -d "~/home_env" ]; then
+        python -m venv home_env
+    fi
+'';
 
 }
