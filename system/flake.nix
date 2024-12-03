9a5e43aebd31b39fa1fcf532fe9c605f9642291b
yejashi <befikirteferi@gmail.com>
2024-12-03 17:06:17 -0500

    flakes whooo!

---
+++ system/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.yejashi = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        # TODO: Fix this madness once you have a better grasp of flakes
+        {
+          environment.systemPackages = [ inputs.zen-browser.packages."x86_64-linux".specific ];
+        }
+      ];
+    };
+  };
+}
