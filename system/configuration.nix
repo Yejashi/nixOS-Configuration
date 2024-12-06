c42c2f15f2b5918128ab85f3d27a6120aab02ff0
yejashi <befikirteferi@gmail.com>
2024-12-06 13:27:29 -0500

    minor

--- system/configuration.nix
+++ system/configuration.nix
@@ -25,8 +25,7 @@
     # Get latest kernel
     kernelPackages = pkgs.linuxPackages_latest;
 
-    # initrd.kernelModules = [ "nvidia-drm" ];
-    #initrd.kernelModules = [ "amdgpu" ];
+    # initrd.kernelModules = [ "amdgpu" ];
 
     initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 
@@ -46,57 +45,58 @@
     enable = true;
   };
 
-  hardware.nvidia = {
+  # Uncomment for nvidia graphics
+   hardware.nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+     # Modesetting is required.
+     modesetting.enable = true;
 
-    nvidiaPersistenced = true;
-    # prime = {
-    #     offload.enable = true;
-    #     #sync.enable = true;
+     nvidiaPersistenced = true;
+     # prime = {
+     #     offload.enable = true;
+     #     #sync.enable = true;
 
-    #     # amdgpuBusId = "PCI:5:0:0";
+     #     # amdgpuBusId = "PCI:5:0:0";
 
-    #     # nvidiaBusId = "PCI:1:0:0";
-    # };
+     #     # nvidiaBusId = "PCI:1:0:0";
+     # };
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
+     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+     # Enable this if you have graphical corruption issues or application crashes after waking
+     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+     # of just the bare essentials.
+     powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    # powerManagement.finegrained = false;
+     # Fine-grained power management. Turns off GPU when not in use.
+     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+     # powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+     # Use the NVidia open source kernel module (not to be confused with the
+     # independent third-party "nouveau" open source driver).
+     # Support is limited to the Turing and later architectures. Full list of 
+     # supported GPUs is at: 
+     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+     # Only available from driver 515.43.04+
+     # Currently alpha-quality/buggy, so false is currently the recommended setting.
+     open = false;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+     # Enable the Nvidia settings menu,
+ 	# accessible via `nvidia-settings`.
+     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-  };
+     # Optionally, you may need to select the appropriate driver version for your specific GPU.
+     # package = config.boot.kernelPackages.nvidiaPackages.stable;
+     package = config.boot.kernelPackages.nvidiaPackages.production;
+   };
 
-#   boot.blacklistedKernelModules = [
-#     "nouveau"
-#     "rivafb"
-#     "nvidiafb"
-#     "rivatv"
-#     "nv"
-#     "uvcvideo"
-#   ];
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "rivafb"
+    "nvidiafb"
+    "rivatv"
+    "nv"
+    "uvcvideo"
+  ];
 
   networking.hostName = "yejashi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -131,7 +131,7 @@
       # Enable the X11 windowing system.
       enable = true;
 
-      videoDrivers = [ "nvidia" "amdgpu"];
+      videoDrivers = [ "nvidia" ];
 
       displayManager = {
         gdm.enable = true;
